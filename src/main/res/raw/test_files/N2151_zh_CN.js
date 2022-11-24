3fdf88d03a1c3a546930458a0da49900e95e8506
123456lj7q <41822383+123456lj7q@users.noreply.github.com>
2022-11-24 22:30:21 +0800

    Delete src/main/res/raw directory

--- src/main/res/raw/test_files/N2151_zh_CN.js
+++
@@ -1,6 +0,0 @@
-vpn_eval((function(){
-//N2151zh_CN资源文件
-;(function($){$.i18n.map = $.i18n.map || {};$.extend($.i18n.map,{'kcxszc':'课程学时组成','xkbz':'选课备注','location':'地点','zhxs':'周学时','jxb':'教学班','jszc':'教师职称','xszd':'显示字段','time':'时间','khfs':'考核方式','xf':'学分'});}(jQuery))
-
-}
-).toString().slice(12, -2),"");
